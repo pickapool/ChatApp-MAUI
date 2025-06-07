66faa2305342e2c4aeea214e2d1e19c12c4f43f6
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-07 12:55:18 +0800

    Web API - add firebase admin and swagger

---
+++ WebAPI/Controllers/FirebaseAuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/auth/[controller]")] 
+    public class FirebaseAuthController : Controller
+    {
+       
+    }
+}
