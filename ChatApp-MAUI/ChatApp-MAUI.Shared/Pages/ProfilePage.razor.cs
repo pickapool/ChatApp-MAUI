7d65b4f17eb7478e64dec98e20aee703064a7b68
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-25 19:05:39 +0800

    Separate code behind and markup profile page

---
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Pages/ProfilePage.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_MAUI.Shared.Pages
+{
+    partial class ProfilePageBase : ComponentBase
+    {
+
+    }
+}
