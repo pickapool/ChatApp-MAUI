b4b75f38b299af2e0bf3b599053fdaf73e3c1366
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-25 19:17:43 +0800

    Separate code and markup mainlayout

---
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Layout/MainLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChatApp_MAUI.Shared.Layout
+{
+    public partial class MainLayoutBase : LayoutComponentBase
+    {
+        protected ElementReference reference;
+        protected string email = string.Empty, password = string.Empty;
+        protected bool _open = false, isLoading = false, isShow = false, isRegistration = false;
+        protected bool _isToggling = false;
+
+        protected async Task ToggleDrawer()
+        {
+            if (_isToggling) return;
+
+            _isToggling = true;
+            _open = !_open;
+
+            await Task.Delay(300); // debounce delay
+            _isToggling = false;
+        }
+    }
+}
