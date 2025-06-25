3421035264863e62764e01b03cadd23bdc09df14
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-25 19:02:22 +0800

    Separate markup and code behind camera page

---
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Pages/CameraPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using ChatApp_MAUI.Shared.Services.NavigationServices;
+using ChatApp_MAUI.Shared.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_MAUI.Shared.Pages
+{
+    public partial class CameraPageBase : ComponentBase
+    {
+        //Service
+        [Inject] protected INavigationService _navigationService { get; set; } = default!;
+        [Inject] protected IFormFactor _formFactor { get; set; } = default!;
+        protected bool isAndroid = false;
+        protected override void OnInitialized()
+        {
+            if (platform.ToLower().Contains("android") || platform.ToLower().Contains("ios"))
+            {
+                isAndroid = true;
+                _navigationService.GoToCameraPage(new Microsoft.Maui.Controls.Page());
+            }
+            else
+            {
+                isAndroid = false;
+            }
+        }
+        private string factor => _formFactor.GetFormFactor();
+        private string platform => _formFactor.GetPlatform();
+    }
+}
