d8f27c89c7e726d4d24651b7fac58b031a2c8a11
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-26 18:41:16 +0800

    check

--- ChatApp-MAUI/ChatApp-MAUI.Shared/Components/ProfileContainer.razor.cs
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Components/ProfileContainer.razor.cs
@@ -21,7 +21,7 @@ namespace ChatApp_MAUI.Shared.Components
 
         protected AuthTokenModel user;
         protected bool _open;
-        protected string DefaultPhoto = "/images/blank-profile.webp";
+        protected string DefaultPhoto = "/images/blank-profile.png";
         protected void ToggleOpen() => _open = !_open;
         protected bool isLoading = true;
         protected override async Task OnInitializedAsync()
