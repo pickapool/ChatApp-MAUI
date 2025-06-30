7cdfc4f527692fa7ffa86ead402a5c4b884a3aaf
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-30 12:02:17 +0800

    Get chat rooms

---
+++ WebAPI/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+﻿using ChatApp_MAUI.Shared.Models;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/message/")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly FirestoreDb _firestoreDb;
+        private readonly IConfiguration _configuration;
+        public MessagesController(IConfiguration config) {
+            _configuration = config;
+            _firestoreDb = FirestoreDb.Create(_configuration["FireStoreDbProjectId"]);
+        }
+        [Authorize]
+        [HttpHead]
+        [Route("getchatroom")]
+        public async Task<IActionResult> GetMessageAsync([FromQuery] string uid)
+        {
+            try
+            {
+                var query = _firestoreDb.Collection("ChatRooms")
+                         .WhereArrayContains("Members", uid);
+                         //for indivual message .OrderByDescending("Messages.TimeStamp");
+
+                var snapshot = await query.GetSnapshotAsync();
+                var chatRooms = snapshot.Documents
+                    .Select(doc => doc.ConvertTo<ChatRoomModel>())
+                    .ToList();
+                return Ok(chatRooms);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+    }
+}
