1684aa6ad7733d3ac8d1b370a22241ab8f8c2db7
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-30 12:21:43 +0800

    Friends model and friends controller

---
+++ WebAPI/Controllers/FriendsController.cs
@@ -0,0 +1,73 @@
+﻿using ChatApp_MAUI.Shared.Models;
+using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/friends/")]
+    public class FriendsController : ControllerBase
+    {
+        private readonly FirestoreDb _firestoreDb;
+        private readonly IConfiguration _configuration;
+        public FriendsController(IConfiguration config)
+        {
+            _configuration = config;
+            _firestoreDb = FirestoreDb.Create(_configuration["FireStoreDbProjectId"]);
+        }
+        [HttpGet]
+        [Route("getfriends")]
+        public async Task<IActionResult> GetFriendsAsync([FromQuery] string uid)
+        {
+            try
+            {
+                var query = _firestoreDb.Collection("Friends")
+                          .WhereEqualTo("From", uid)
+                          .WhereEqualTo("IsAccepted", true);
+                var snapshot = await query.GetSnapshotAsync();
+                var friendsList = snapshot.Documents
+                    .Select(doc => doc.ConvertTo<FriendsModel>())
+                    .ToList();
+                return Ok(friendsList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("addfriend")]
+        public async Task<IActionResult> AddFriendAsync([FromBody] FriendsModel friendsModel)
+        {
+            try
+            {
+                var docRef = _firestoreDb.Collection("Friends").Document();
+                friendsModel.Id = docRef.Id;
+                await docRef.SetAsync(friendsModel);
+                return Ok(new { Message = "Friend request sent successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+        [Authorize]
+        [HttpDelete]
+        [Route("adfriend")]
+        public async Task<IActionResult> DeleteFriendAsync([FromBody] FriendsModel friendsModel)
+        {
+            try
+            {
+                var docRef = _firestoreDb.Collection("Friends").Document(friendsModel.Id);
+                await docRef.DeleteAsync();
+                return Ok(new { Message = "Friend request sent successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+    }
+}
