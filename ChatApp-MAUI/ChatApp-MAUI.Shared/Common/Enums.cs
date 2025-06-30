ed6512cf43760bc283748afdadda1551a0e68edd
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-06-30 11:39:37 +0800

    Add models for Firestore daabase

---
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Common/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_MAUI.Shared.Common
+{
+    public class Enums
+    {
+        public enum MessageType
+        {
+            Text,
+            Image,
+            Video,
+            Audio,
+            File
+        }
+    }
+}
---
+++ ChatApp-MAUI/ChatApp-MAUI.Shared/Models/ChatRoomModel.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp_MAUI.Shared.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_MAUI.Shared.Models
+{
+    public class ChatRoomModel
+    {
+        public string? uid { get; set; }
+        public List<string>? members { get; set; }
+        public string? Text { get; set; }
+        public Enums.MessageType Type { get; set; }
+    }
+}
